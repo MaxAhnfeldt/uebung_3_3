6b40bc69c63e7fa47b56b851127e3866463ae6de
MaxAhnfeldt <maximilian.ahnfeldt@nordakademie.de>
2016-10-31 12:23:14 +0100

    blabla

--- src/BMIBerechnung.java
+++ src/BMIBerechnung.java
@@ -3,16 +3,15 @@ public class BMIBerechnung {
 
 	public static void main(String[] args) {
 
-		int Geschlecht = 1;
-		double Alter = 48;
-		double Gewicht = 89;
-		double Groeße = 1.92;
+		int Geschlecht = 0;// 1 ist männlich, 2 ist weiblich
+		double Alter = 0;
+		double Gewicht =0;
+		double Groeße =0;
 		double BMI = 0;
 
 		BMI = (Gewicht / (Groeße * Groeße));
-		
 
-		if (Geschlecht == 1) {//ist männlich
+		if (Geschlecht == 1) {
 			if (Alter > 64) {
 				if (BMI > 30) {
 					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
@@ -23,9 +22,7 @@ public class BMIBerechnung {
 						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
 					}
 				}
-			}
-		} else if
-			(Alter >= 55) {
+			} else if (Alter >= 55) {
 				if (BMI > 29) {
 					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
 				} else {
@@ -35,9 +32,7 @@ public class BMIBerechnung {
 						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
 					}
 				}
-			}
-		 else
-			if (Alter >= 45) {
+			} else if (Alter >= 45) {
 				if (BMI > 28) {
 					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
 				} else {
@@ -47,6 +42,95 @@ public class BMIBerechnung {
 						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
 					}
 				}
+			} else if (Alter >= 35) {
+				if (BMI > 27) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 22) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (Alter >= 25) {
+				if (BMI > 26) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 21) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (BMI > 25) {
+				System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+			} else {
+				if (BMI >= 20) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+				} else {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+				}
+			}
+		} else {
+			if (Alter > 64) {
+				if (BMI > 29) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 24) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (Alter >= 55) {
+				if (BMI > 28) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 23) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (Alter >= 45) {
+				if (BMI > 27) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 22) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (Alter >= 35) {
+				if (BMI > 26) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 21) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (Alter >= 25) {
+				if (BMI > 25) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+				} else {
+					if (BMI >= 20) {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+					} else {
+						System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+					}
+				}
+			} else if (BMI > 24) {
+				System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu hoch!");
+			} else {
+				if (BMI >= 19) {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu okay!");
+				} else {
+					System.out.println("Ihr BMI beträgt: " + BMI + "und ist zu niedrig!");
+				}
 			}
 		}
 	}
+}
